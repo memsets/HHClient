419ee4040020c4e202317ed3064af0b8d06acb6d
memsets <memsets@gmail.com>
2021-05-09 22:55:19 +0300

    Create HH work client class.

--- HHClient/Form1.cs
+++ HHClient/Form1.cs
@@ -16,79 +16,26 @@ using System.Windows.Forms;
 namespace HHClient {
     public partial class Form1 : Form 
     {
-
-        private CookieContainer cookies = new();
-        private Token token = new();
-        private string accessToken;
-        private string client_id;
-        private string client_secret;
-
-        public Form1() 
+        private HHClient client = new();
+        
+        public Form1()
         {
-            accessToken = "SO0RTSMPC8RBE7SB5HKU04KVRI88GLK1PAF5OVNURULRVRD65715GUL2PU88NNQH";
-            client_id = "KEE06R38HQV44AOK5MSU0S0A8MEN7DRMSS4LNEGAF3ET0EUVIND8GITQDC8RR4JH";
-            client_secret = "JNJ2SEGE66SL324O4PATMCIEE7FJ8UHBBSQ42879U1E7Q7A8SJBLLTHJE0UMT34G";
             InitializeComponent();
         }
 
-        private async void Auth()
-        {
-            var uri = new Uri("https://hh.ru/oauth/token");
-            HttpClientHandler handler = new HttpClientHandler();
-            var client = new HttpClient(handler);
-            handler.CookieContainer = cookies;
-            handler.UseCookies = true;
-
-            var stringContent = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("grant_type", "client_credentials"),
-                new KeyValuePair<string, string>("client_id", client_id),
-                new KeyValuePair<string, string>("client_secret", client_secret)
-            });
-
-            var response = await client.PostAsync(uri, stringContent);
-            richTextBox2.Text = await response.Content.ReadAsStringAsync();
-            token = JsonSerializer.Deserialize<Token>(richTextBox2.Text);
-
-            JsonArea.Text = token.access_token;
-        }
-
-        private async void HHGet_Click(object sender, EventArgs e) 
+        private void HHGet_Click(object sender, EventArgs e) 
         {
             CookieArea.Text = "";
             richTextBox2.Text = "";
-            string uri = "https://api.hh.ru/vacancies/?text=программист&per_page=100";
-            //string uri = "https://api.hh.ru/vacancies/pages";           
-            HttpClientHandler handler = new HttpClientHandler();
-            handler.CookieContainer = cookies;
-            handler.UseCookies = true;
-
-            var client = new HttpClient(handler);
-
-            client.DefaultRequestHeaders.Add("User-Agent", "api-test-agent");
-            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-            
-            
-
-            var response = await client.GetAsync(uri);
-            JsonArea.Text = await response.Content.ReadAsStringAsync();
-            HeadersArea.Text = response.Headers.ToString();
-            var content = JsonSerializer.Deserialize<Rootobject>(JsonArea.Text);
-            richTextBox2.Text += content.pages;
 
             for(int i = 0; i < 1; i++) // 5 is debug 
             {
-                uri = "https://api.hh.ru/vacancies/?text=\"программист C++\"&per_page=100&page=" + i.ToString();
-                response = await client.GetAsync(uri);
-                JsonArea.Text = await response.Content.ReadAsStringAsync();
-
-
+                var content = client.GetVacancies("Программист С++", "100", "1");
                 foreach (var item in content.items)
                 {
                     richTextBox2.Text += item.name;
                     richTextBox2.Text += " " + item.alternate_url + "\n";
                 }
-
             }
             //var url = new Uri(SearchString.Text);
             //richTextBox2.Text = cookies.GetCookies(url).Count().ToString();
@@ -99,7 +46,7 @@ namespace HHClient {
 
         private void AuthButton_Click(object sender, EventArgs e)
         {
-            Auth();
+            richTextBox2.Text = client.HHAplicationAuth().access_token;
         }
 
         private void richTextBox2_LinkClicked(object sender, LinkClickedEventArgs e)
---
+++ HHClient/HHClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HHClient
+{
+    class HHClient
+    {
+        private CookieContainer cookies = new();
+        private HttpClientHandler handler = new();
+        private HttpClient client;
+        private Uri uri;
+        private string clientId;
+        private string clientSecret;
+        private string accessToken;
+
+        public HHClient()
+        {
+            handler.CookieContainer = cookies;
+            handler.UseCookies = true;
+            client = new HttpClient(handler);
+
+            clientId = "KEE06R38HQV44AOK5MSU0S0A8MEN7DRMSS4LNEGAF3ET0EUVIND8GITQDC8RR4JH";
+            clientSecret = "JNJ2SEGE66SL324O4PATMCIEE7FJ8UHBBSQ42879U1E7Q7A8SJBLLTHJE0UMT34G";
+            accessToken = "SO0RTSMPC8RBE7SB5HKU04KVRI88GLK1PAF5OVNURULRVRD65715GUL2PU88NNQH";
+        }
+
+        public Token HHAplicationAuth()
+        {
+            uri = new Uri("https://hh.ru/oauth/token");
+            var stringContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                new KeyValuePair<string, string>("client_id", clientId),
+                new KeyValuePair<string, string>("client_secret", clientSecret)
+            });
+
+            var response = client.PostAsync(uri, stringContent);
+            var text = response.Result.Content.ReadAsStringAsync().Result;
+
+            var token = JsonSerializer.Deserialize<Token>(text);
+
+            return token;
+        }
+
+        public Rootobject GetVacancies(string searchText, string perPage, string page = null)
+        {
+            uri = new Uri("https://api.hh.ru/vacancies/?text=" + searchText + 
+                "&per_page=" + perPage + "&page=" + page);
+            
+            client.DefaultRequestHeaders.Add("User-Agent", "api-test-agent");
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+            
+            var result = client.GetAsync(uri).Result.Content.ReadAsStringAsync().Result;
+            var rootObject = JsonSerializer.Deserialize<Rootobject>(result);
+            return rootObject;
+        }
+    }
+}
